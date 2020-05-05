a07e43c2fb8ec887596cc2f8fef0cbd27f8cbb45
Morteza-am <40890672+Morteza-am@users.noreply.github.com>
2020-05-05 11:15:18 +0430

    ehlo command fixed

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ subject = input("Enter Subject:")
 secure_connection = bool(input("Secure Connection? True/False? "))
 message_data= input("Enter your message:")
 message = "To:" + receiver_email + "\nFrom:" + sender_email + "\nsubject:" + subject + "\n\n" + message_data;
-
+server = smtplib.SMTP(smtp_server,port)
 # Try to log in to server and send email
 try:
     server = smtplib.SMTP(smtp_server,port)
-    server.ehlo(smtp_server)
+    server.ehlo()
     if secure_connection:
         server.starttls()
-        server.ehlo(smtp_server)
+        server.ehlo()
     server.sendmail(sender_email, receiver_email, message)
     print("\nMessage Sent Successfully.")
 except Exception as e:
